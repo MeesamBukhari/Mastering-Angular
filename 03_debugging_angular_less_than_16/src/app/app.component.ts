1d5317728bdc00bc375fdedd058df28cd10236e8
MeesamBukhari <meesamturab@gmail.com>
2024-07-29 12:15:07 +0500

    debugging code in the browser using sourceMaps

--- 03_debugging_angular_less_than_16/src/app/app.component.ts
+++ 03_debugging_angular_less_than_16/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   servers = [];
@@ -13,7 +13,6 @@ export class AppComponent {
   }
 
   onRemoveServer(id: number) {
-    const position = id + 1;
-    this.servers.splice(position, 1);
+    this.servers.splice(id, 1);
   }
 }
